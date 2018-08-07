c03f64fc7b128f17499bd949c4d2e031d2cb47b6
Cristian Echeverri <cristiancamilo14041999@gmail.com>
2018-08-07 10:54:15 -0500

    Eliminadas librerias no usadas

--- routes/routes.js
+++ routes/routes.js
@@ -4,6 +4,11 @@ module.exports = function (app) {
         res.render('index'); //Show login
     });
 
+    // app.post('/', function (req, res) {
+    //     req.session.nombre = req.body.nombre;
+    //     res.redirect('home'); //Show login
+    // });
+
     app.get('/home', function (req, res) {
         res.render('home'); //Show home
     });
@@ -25,31 +30,51 @@ module.exports = function (app) {
     });
 
     app.get('/saloon', function (req, res) {
-        res.render('saloon'); //Lets add a new institution
+        res.render('saloon'); //show the registred rooms
     });
 
     app.get('/newsaloon', function (req, res) {
-        res.render('newsaloon'); //Lets add a new institution
+        res.render('newsaloon'); //Lets add a new room
     });
 
     app.get('/category', function (req, res) {
-        res.render('category'); //Lets add a new institution
+        res.render('category'); //Show the registred categories
     });
 
     app.get('/newcategory', function (req, res) {
-        res.render('newcategory'); //Lets add a new institution
+        res.render('newcategory'); //Lets add a new category
     });
 
     app.get('/shelf', function (req, res) {
-        res.render('shelf'); //Lets add a new institution
+        res.render('shelf'); //Show the registred shelfs
     });
 
     app.get('/newshelf', function (req, res) {
-        res.render('newshelf'); //Lets add a new institution
+        res.render('newshelf'); //Lets add a new shelf
     });
 
     app.get('/catalog', function (req, res) {
-        res.render('catalog'); //Lets add a new institution
+        res.render('catalog'); //Show all the added books
+    });
+
+    app.get('/loan', function (req, res) {
+        res.render('loan'); //Show all the loans
+    });
+
+    app.get('/loanpending', function (req, res) {
+        res.render('loanpending'); //Show all the pending loans
+    });
+
+    app.get('/loanreservation', function (req, res) {
+        res.render('loanreservation'); //Show all the reservations
+    });
+
+    app.get('/report', function (req, res) {
+        res.render('report'); //Show the reports of books
+    });
+
+    app.get('/settings', function (req, res) {
+        res.render('settings'); //Lets change some options of the system
     });
 
     app.use(function (req, res, next) {
